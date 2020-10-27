46d544b82bfdaf7eb3c8b44fa09c3f70ba45369b
Pablo Garrido <pablogs9@gmail.com>
2020-10-27 16:42:53 +0100

    Added servo demo

--- src/microros_moveit2_demo.cpp
+++ src/microros_moveit2_demo.cpp
@@ -1,4 +1,5 @@
 #include <thread>
+#include <cmath>
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/moveit_cpp/moveit_cpp.h>
 #include <moveit/moveit_cpp/planning_component.h>
@@ -32,10 +33,24 @@ public:
     // Init micro-ROS subscribers
     auto trigger_callback = [this](const std_msgs::msg::Empty::SharedPtr msg) -> void
     {
-      arm_->setGoal(last_pose_, "panda_link8");
+      geometry_msgs::msg::PoseStamped goal_pose = last_pose_;
+
+      double mod = sqrt( 
+         goal_pose.pose.orientation.x * goal_pose.pose.orientation.x + 
+         goal_pose.pose.orientation.y * goal_pose.pose.orientation.y +
+         goal_pose.pose.orientation.z * goal_pose.pose.orientation.z +
+         goal_pose.pose.orientation.w * goal_pose.pose.orientation.w  
+      );
+
+      goal_pose.pose.orientation.x *= 1/mod;
+      goal_pose.pose.orientation.y *= 1/mod;
+      goal_pose.pose.orientation.z *= 1/mod;
+      goal_pose.pose.orientation.w *= 1/mod;
+
+      arm_->setGoal(goal_pose, "panda_link8");
 
       // Run actual plan
-      RCLCPP_INFO(LOGGER, "Plan to goal");
+      RCLCPP_INFO(LOGGER, "Plan to goal: %f %f %f", goal_pose.pose.orientation.x, goal_pose.pose.orientation.y, goal_pose.pose.orientation.z);
       const auto plan_solution = arm_->plan();
       if (plan_solution)
       {
@@ -46,14 +61,16 @@ public:
 
     auto tf_callback = [this](const tf2_msgs::msg::TFMessage::SharedPtr msg) -> void
     {
-      last_pose_.pose.orientation.w = msg->transforms[0].transform.rotation.w;
-      last_pose_.pose.orientation.x = msg->transforms[0].transform.rotation.x;
-      last_pose_.pose.orientation.y = msg->transforms[0].transform.rotation.y;
-      last_pose_.pose.orientation.z = msg->transforms[0].transform.rotation.z;
-      
-      last_pose_.pose.position.x = msg->transforms[0].transform.translation.x;
-      last_pose_.pose.position.y = msg->transforms[0].transform.translation.y;
-      last_pose_.pose.position.z = msg->transforms[0].transform.translation.z;
+      if(msg->transforms[0].child_frame_id == "/inertial_unit"){
+        last_pose_.pose.orientation.w = msg->transforms[0].transform.rotation.w;
+        last_pose_.pose.orientation.x = msg->transforms[0].transform.rotation.x;
+        last_pose_.pose.orientation.y = msg->transforms[0].transform.rotation.y;
+        last_pose_.pose.orientation.z = msg->transforms[0].transform.rotation.z;
+        
+        last_pose_.pose.position.x = msg->transforms[0].transform.translation.x;
+        last_pose_.pose.position.y = msg->transforms[0].transform.translation.y;
+        last_pose_.pose.position.z = msg->transforms[0].transform.translation.z;
+      }
     };
 
     trigger_sub_ = node_->create_subscription<std_msgs::msg::Empty>("trigger_moveit2", rclcpp::QoS(rclcpp::KeepLast(1)), trigger_callback);
